201cc64ebda39ea1deb5bafbeb8c310a7cda3ec9
Nikita-Syabruyk <90602552+Nikita-Syabruyk@users.noreply.github.com>
2022-07-05 15:10:56 +0300

    Add files via upload

---
+++ obuchenie.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cin >> n;
+	int k = 1;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	
+	for (int i = 1; i < n; i=i+2) {
+		int j = arr[i-1];
+		arr[i - 1] = arr[i];
+		arr[i] = j;
+		if (arr[i + 2] == NULL) {
+			break;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+	delete[]arr;
+}
